ed8e83130c04f62726b043020ee308652ed861bc
wesley <wesley.yang789@hotmail.com>
2022-04-11 13:25:45 +0800

    learn namespace, type, interface....

--- class_and_interface.ts
+++ class_and_interface.ts
@@ -68,3 +68,91 @@ class Employee extends Person {
 
 let howard = new Employee("Howard", "Sales")
 console.log(howard.getElevatorPitch())
+
+/// interface
+///
+/// define types
+///
+
+interface Point {
+    x: number;
+    y: number;
+    z?:number;
+}
+
+let pt: Point = {x: 1, y: 2, z: 1}
+console.log(`pt.x -> ${pt.x} pt.y -> ${pt.y} pt.z -> ${pt.z}`)
+
+class NewPoint implements Point {
+    x: number;
+    y: number;
+    constructor(x: number, y: number) {
+	this.x = x;
+	this.y = y;
+    }
+
+    public getPoint(): object {
+	return {
+	    x: this.x,
+	    y: this.y,
+	}
+    }
+}
+
+const p = new NewPoint(3, 4)
+const p1 = p.getPoint()
+//console.log(`p1.x -> ${p1.x} p1.y -> ${p1.y}`)
+//console.log(p1.x) // WHY
+
+/// interface & interface
+///
+interface Shape {
+    color: string;
+}
+
+interface Square extends Shape {
+    sideLength: number;
+}
+
+class Rectangle implements Square {
+    color:string;
+    sideLength: number;
+    constructor(color: string, sideLength: number) {
+	this.color = color;
+	this.sideLength = sideLength;
+    }
+
+    public printAll(): void {
+	console.log(`color ${this.color} sidelength ${this.sideLength}`)
+    }
+}
+
+/// type
+/// 定义一个新的类型
+
+type myType = string | number | boolean
+const foo: myType = "Hello World!!!"
+
+/// type defined a function
+
+type hello = (msg:string) => void
+
+/// type defined a object
+type WebSite = {
+    url:string;
+    title:string;
+}
+
+// namespace
+//
+
+namespace Validator {
+
+    let name = "Validator";
+
+    export getName = function() {
+	return name
+    }
+}
+
+console.log(Validator.name)
